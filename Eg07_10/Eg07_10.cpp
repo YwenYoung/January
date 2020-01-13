3e466520702eb3460f7e964f666dce9789b4a050
文成 杨 <boy_yec@hotmail.com>
2020-01-13 19:21:00 +0800

    2020-1-13 19:20:58

--- Eg07_10/Eg07_10.cpp
+++ Eg07_10/Eg07_10.cpp
@@ -9,16 +9,16 @@ using namespace std;
 int main()
 {
 	default_random_engine engine(static_cast<unsigned int>(time(0)));
-	uniform_int_distribution<unsigned int>randomInt(0, 6);
+	uniform_int_distribution<unsigned int>randomInt(1, 6);
 
 	const size_t arraySize = 7;
 	array<unsigned int, arraySize> frequency = {};
 
-	for (unsigned int roll = 1; roll <= 6000000; ++roll)
+	for (unsigned int roll = 0; roll < 6000000; ++roll)
 		++frequency[randomInt(engine)];
 
 	cout << "Face" << setw(13) << "Frequency" << endl;
 
-	for (size_t face = 0; face < frequency.size(); ++face)
+	for (size_t face = 1; face < frequency.size(); ++face)
 		cout << setw(4) << face << setw(13) << frequency[face] << endl;
 }
