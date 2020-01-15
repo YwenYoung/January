f81db46db26f7aa983692763457d251c12226949
文成 杨 <boy_yec@hotmail.com>
2020-01-15 21:28:31 +0800

    测试基于计数器和基于范围

--- Eg07_10/Eg07_10.cpp
+++ Eg07_10/Eg07_10.cpp
@@ -1,4 +1,6 @@
+/*
 //用array对象元素当做计数器使用
+
 #include <iostream>
 #include <iomanip>
 #include <array>
@@ -21,4 +23,50 @@ int main()
 
 	for (size_t face = 1; face < frequency.size(); ++face)
 		cout << setw(4) << face << setw(13) << frequency[face] << endl;
+}
+*/
+
+//另一个测试，区分基于计数器方法的赋值和基于范围方法的赋值
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+	const int i = 2;
+	const int j = 3;
+
+	array<array<int, j>, i> t = {};
+
+	for (int m = 0; m < i; m++)
+	{
+		for (int n = 0; n < 3; n++)
+		{
+			t[m][n] = 1;
+
+			cout << t[m][n] << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (auto &row : t)
+	{
+		for (auto &column : row)
+		{
+			column = 2;
+
+			cout << column << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (int m = 0; m < i; m++)
+	{
+		for (int n = 0; n < 3; n++)
+		{
+			cout << t[m][n] << ' ';
+		}
+		cout << '\n';
+	}
+
 }
