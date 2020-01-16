b2aa99268dc21fdc20e2873f006f94c159d8bf2b
文成 杨 <boy_yec@hotmail.com>
2020-01-16 13:09:16 +0800

    1

--- Ex07_10/Ex07_10.cpp
+++ Ex07_10/Ex07_10.cpp
@@ -10,7 +10,6 @@ int main()
 	int salaries[11] = {};
 	cout << fixed << showpoint;
 	wages(salaries);
-	display(salaries);
 	return 0;
 }
 
@@ -20,4 +19,24 @@ void wages(int money[])
 	double i = 0.09;
 	cout << "Enter employee gross sales (-1 to end): ";
 	cin >> sales;
+
+	while (sales != -1)
+	{
+		double salary = 200.0 + sales * i;
+		cout << setprecision(2) << "Employee Commission is $" << salary << '\n';
+		int x = static_cast<int>(salary) / 100;
+		money[(x < 10 ? x : 10)]++;			//记录了不同收入等级的个数
+		cout << "\nEnter employee gross sales (-1 to end): ";
+		cin >> sales;
+	}
+}
+
+void display(const int dollars[])
+{
+	cout << "Employee in the range: ";
+
+	for (int n = 2; n < 10; n++)
+		cout << "\n$" << n << "00-$" << n << "99 : " << dollars[n];
+
+	cout << "\nOver $1000: " << dollars[10] << endl;
 }
